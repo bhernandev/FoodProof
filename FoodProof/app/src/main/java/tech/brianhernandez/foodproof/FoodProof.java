182a0ecfb4cf6e396bf283835ff93369671286f3
bhernandez-cs <brian@hernandez-cs.com>
2017-01-18 16:32:51 -0500

    Added basic Android project
    Added basic Android project

---
+++ FoodProof/app/src/main/java/tech/brianhernandez/foodproof/FoodProof.java
@@ -0,0 +1,13 @@
+package tech.brianhernandez.foodproof;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodProof extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_proof);
+    }
+}
